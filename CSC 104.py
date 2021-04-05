91865407caabaf03e5e092e0e01cf9e24767054e
Ramalan83 <81994096+Ramalan83@users.noreply.github.com>
2021-04-05 21:37:26 +0100

    Add files via upload

---
+++ CSC 104.py
@@ -0,0 +1,17 @@
+name = "Ramalan"
+Dept = "Computer Science"
+Print = (name,"\n", dept)
+Scare = "boo!'
+print (Scare)
+n1 = 3
+n2 = 5
+n3 = n1 + n2
+Print ("n1 =",n1,"\nn2=",n2,"\nn3 = n1 + n2", "\nn3 = " ,n1+n2,"\n")
+Rem =8/0.5
+print (Rem)
+total = 4 + 5
+print (Total)
+rem = 8 % 3
+print (rem)
+X = (2*4)*(4+2)
+print (X)
